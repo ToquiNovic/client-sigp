2b1db74f0baf3a5dfc7009588ee247172e7b2926
ToquiNovic <j.toquica@udla.edu.co>
2023-04-21 19:28:57 -0500

    singup

---
+++ src/components/TipoIDSelect/index.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { setTipos, setTipoSeleccionado } from '../../redux/features/TipoIDSlice';
+import { getTipos } from '../../services/tipoID';
+
+const TipoIDSelect = () => {
+  const dispatch = useDispatch();
+  const tipos = useSelector((state) => state.tipoID.tipos) || [];
+  const tipoSeleccionado = useSelector((state) => state.tipoID.tipoSeleccionado);
+
+  useEffect(() => {
+    getTipos().then((response) => {
+      dispatch(setTipos(response.msg));
+    });
+  }, []);
+
+  const handleChange = (event) => {
+    dispatch(setTipoSeleccionado(event.target.value));
+    console.log(event.target.value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="tipo">Tipo de Identificacion</InputLabel>
+      <Select
+        labelId="tipo"
+        name="tipo"
+        label="Tipo de Identificacion"
+        value={tipoSeleccionado || ''}
+        onChange={handleChange}
+      >
+        {tipos.map((tipo) => (
+          <MenuItem key={tipo.TIID_ID_TIPOID} value={tipo.TIID_ID_TIPOID}>
+            {tipo.TIID_NOMBRE_TIPOID}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default TipoIDSelect;
